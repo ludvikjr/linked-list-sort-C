458ee86e7ec16572cafb130b8fab267fc21227f4
Ludvik <ludvik.jakub.roubicek@gmail.com>
2023-01-08 20:45:21 +0100

    Implement bubble sort

--- main.c
+++ main.c
@@ -63,6 +63,39 @@ void freeList(TITEM *l)
     }
 }
 
+TITEM * bubbleSort(TITEM * l, int length)
+{
+    TITEM * head = l;
+    for (int i = 0; i < length; i++)
+    {
+        head = l;
+        TITEM * head2 = l;
+        for (int j = 0; j < length - i; j++)
+        {
+            if (head->data >= head2->data)
+            {
+                int temp = head2->data;
+                head2->data = head->data;
+                head->data = temp;
+                head = head2;
+            } else {
+                head = head2;
+            }
+            head2 = head2->next;
+        }
+        
+    }
+
+
+    return head;
+    
+}
+
+TITEM * sort (TITEM * l, int length)
+{
+    return bubbleSort(l, length);
+}
+
 int main()
 {
     TITEM *head = (TITEM *)malloc(sizeof(TITEM));
@@ -78,9 +111,15 @@ int main()
         return 1;
     }
 
+/*
     printf("Choose sorting algorithm for a linked list\n");
     printf("1 - Bubble sort\n2 - Merge sort\n");
+*/
+    TITEM * sortedList = sort(head, length);
 
-    freeList(head);
+    printf("Sorted list:\n");
+    printList(sortedList);
+
+    freeList(sortedList);
     return 0;
 }
